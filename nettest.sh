f42596110d6ecac7ab70377715f0bcfb9b1988a7
hunter-sn <63088281+hunter-sn@users.noreply.github.com>
2021-06-28 21:12:48 +0800

    Update nettest.sh

--- nettest.sh
+++ nettest.sh
@@ -1,4 +1,5 @@
 apt install -y wget zip
 wget https://cdn.ipip.net/17mon/besttrace4linux.zip&&unzip besttrace* -d ./BestTrace/
 rm besttrace4linux.zip&&chmod +x ./BestTrace/besttrace
-./BestTrace/besttrace
+read -p "输入ip：" ip
+./BestTrace/besttrace $ip
